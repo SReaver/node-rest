6444678fafd8d41733ffa85fd3bb6065dedb4b6e
Reaver <reaver@Slava-Mac.local>
2019-10-29 23:25:37 +0600

    Only REST done

---
+++ app.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg') {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+}
+app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+
+app.use((error, req, res, nex) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message, data });
+});
+
+mongoose.connect('mongodb+srv://sergey:timhib-vigxo5-kiQsyp@cluster0-i5rut.mongodb.net/messages', { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(result => {
+    app.listen(8080);
+  }).then(() => console.log('app started at port 8080')
+  ).catch(err => console.log(err)
+  );
